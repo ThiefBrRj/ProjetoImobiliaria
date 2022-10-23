3d304d38aab0d4361aed6d737c34e5ed912eb787
Thiago Fagundes <thiagofagundesdossantos@gmail.com>
2022-10-23 19:43:45 -0300

    cors error handled v3

--- src/main/java/br/edu/iff/ProjetoImobiliaria/security/MyApp.java
+++ src/main/java/br/edu/iff/ProjetoImobiliaria/security/MyApp.java
@@ -1,6 +1,5 @@
 package br.edu.iff.ProjetoImobiliaria.security;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -13,7 +12,4 @@ public class MyApp extends SpringBootServletInitializer {
             return application.sources(MyApp.class);
         }
 
-        public static void main(String[] args) {
-            SpringApplication.run(MyApp.class, args);
-        }
 }
