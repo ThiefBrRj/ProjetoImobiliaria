5d4864dacd32353de358668b1220b639f89d814a
Thiago Fagundes <99626328+ThiefBrRj@users.noreply.github.com>
2022-10-22 22:26:44 -0300

    Delete ProjetoImobiliaria directory

--- ProjetoImobiliaria/src/main/java/br/edu/iff/ProjetoImobiliaria/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.edu.iff.ProjetoImobiliaria.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
