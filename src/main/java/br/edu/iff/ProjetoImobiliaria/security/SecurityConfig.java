c75196e1bf4018fb1046b902f452b19e4d36c289
Thiago Fagundes <thiagofagundesdossantos@gmail.com>
2022-10-23 20:44:57 -0300

    cors error handled v14

--- src/main/java/br/edu/iff/ProjetoImobiliaria/security/SecurityConfig.java
+++ src/main/java/br/edu/iff/ProjetoImobiliaria/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package br.edu.iff.ProjetoImobiliaria.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
