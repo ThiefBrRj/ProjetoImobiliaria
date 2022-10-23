7eeaec7cccd0fce50314b369ba2202829353fad9
Thiago Fagundes <thiagofagundesdossantos@gmail.com>
2022-10-23 20:04:04 -0300

    cors error handled v8

--- src/main/java/br/edu/iff/ProjetoImobiliaria/security/SecurityConfig.java
+++ src/main/java/br/edu/iff/ProjetoImobiliaria/security/SecurityConfig.java
@@ -4,21 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable();
+    protected void configure(HttpSecurity security) throws Exception
+    {
+     security.httpBasic().disable();
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
-    }
-
 }
