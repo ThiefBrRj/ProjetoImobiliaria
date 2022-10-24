0f8b1192bb08b8b6ed7e81aa23545968eaeff813
Thiago Fagundes <thiagofagundesdossantos@gmail.com>
2022-10-23 21:40:16 -0300

    cors error handled v15

--- src/main/java/br/edu/iff/ProjetoImobiliaria/model/Pessoa.java
+++ src/main/java/br/edu/iff/ProjetoImobiliaria/model/Pessoa.java
@@ -6,15 +6,12 @@ import java.util.List;
 import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -40,7 +37,7 @@ public abstract class Pessoa implements Serializable {
     @CPF
     private String cpf;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL)
     @NotNull
     @Valid
     private List<Telefone> telefones = new ArrayList<>();
